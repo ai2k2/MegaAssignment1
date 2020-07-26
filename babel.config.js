85c9fb3bfd734858fd318e1c085a3ddc71c152b4
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2020-07-26 20:27:53 +0000

    Prepare 4.0.0 release

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,6 @@ module.exports = {
     ['@babel/preset-env', {
       targets: { node: '8' },
       /*
-       * useBuiltIns configures how @babel/preset-env handles p
+       * useBuiltIns configures how @babel/preset-env handles polyfills.
+       * useBuiltIns: 'usage' - adds specific imports for polyfills when they are used in each file.
+       * useBuiltIns: 'entry' - adds individual requires to different core-js entry points based o
