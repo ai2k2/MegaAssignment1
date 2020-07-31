b48983e4bb18588497a4188088eec8ce06d0254d
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2020-07-31 06:56:40 +0000

    The Homepage

--- babel.config.js
+++ babel.config.js
@@ -5,4 +5,9 @@ module.exports = {
       /*
        * useBuiltIns configures how @babel/preset-env handles polyfills.
        * useBuiltIns: 'usage' - adds specific imports for polyfills when they are used in each file.
-       * useBuiltIns: 'entry' - adds individual requires to different core-js entry points based o
+       * useBuiltIns: 'entry' - adds individual requires to different core-js entry points based on environment.
+       * useBuiltIns: false - does not add polyfills automatically per file
+       * 
+       * Note:
+       * This library (serverless-pg) does not currently require polyfills
+       * but it may in th
