6089d2890e274bc30b0ed68b62c637d88e31cda8
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2020-09-25 22:03:28 +0000

    Add install instructions

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,11 @@
 // For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configurati
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+
+  // Stop running tests after `n` failures
+  // bail: 0,
+
+  // Respect "browser" field in
