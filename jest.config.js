6d31245aa15835ffee5348b7e9fe57ade30191c4
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2020-11-05 18:57:18 +0000

    Simplify README.md

--- jest.config.js
+++ jest.config.js
@@ -81,4 +81,10 @@ module.exports = {
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
 
-  // An array of regexp pat
+  // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
+  // modulePathIgnorePatterns: [],
+
+  // Activates notifications for test results
+  // notify: false,
+
+  /
