a488d692cfb65fe70c129a38f1af2d5b4ac17fe9
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2020-12-13 15:22:12 +0000

    Updating to latest ycmd

--- jest.config.js
+++ jest.config.js
@@ -171,4 +171,9 @@ module.exports = {
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
-  
+    "/node_modules/",
+    "/lib/"
+  ],
+
+  // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
+  // unmockedModu
