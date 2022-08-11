7bab18fd4bde6c0fb114468ee7378dee5bf724cd
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-08-11 11:58:03 +0000

    Updates podspec

--- src/accounting/compound_v2/liquidateBorrow_borrower.ts
+++ src/accounting/compound_v2/liquidateBorrow_borrower.ts
@@ -1,4 +1,11 @@
 import { Map as IMap, List } from 'immutable';
 
 import { GenericTransformOptions } from '../../types';
-import { lotsAndDisposalsFromWithdrawal } from 
+import { lotsAndDisposalsFromWithdrawal } from '../generic/withdrawal';
+
+/*
+ * COMPOUND_LIQUIDATEBORROW_BORROWER
+ *
+ * Creates Disposals associated with a compound COMPOUND_LIQUIDATEBORROW_BORROWER.
+ */
+export const lotsAndDisposalsFromCompoundLiquidate
