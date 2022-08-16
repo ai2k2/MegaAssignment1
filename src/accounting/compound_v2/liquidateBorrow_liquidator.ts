47c5a60d5129559f0ee351e7d62c1fe5fed8d5b5
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-08-16 05:29:39 +0000

    Release 0.0.1

--- src/accounting/compound_v2/liquidateBorrow_liquidator.ts
+++ src/accounting/compound_v2/liquidateBorrow_liquidator.ts
@@ -2,4 +2,11 @@ import { Map as IMap, List } from 'immutable';
 import { BigNumber } from 'bignumber.js';
 
 import TaxLot from '../../taxLot';
-import Dispo
+import Disposal from '../../disposal';
+import { GenericTransformOptions } from '../../types';
+import { transactionUnixNumber, getPriceBigNumber } from '../helpers';
+
+/*
+ * COMPOUND_LIQUIDATEBORROW_LIQUIDATOR
+ *
+ * Creates TaxLots and Dispos
