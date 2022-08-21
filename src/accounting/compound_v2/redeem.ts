4bfa612d6f1445c0bb2d67a77054699b5a5bd64f
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-08-21 15:16:29 +0000

    www-plugins/chrome-binary-plugins: automated update

--- src/accounting/compound_v2/redeem.ts
+++ src/accounting/compound_v2/redeem.ts
@@ -1,3 +1,9 @@
 import { Map as IMap, List } from 'immutable';
 
-import { Price, PriceMethod, LocalCurrency, ImmutableMap, ITransaction } from '../../
+import { Price, PriceMethod, LocalCurrency, ImmutableMap, ITransaction } from '../../types';
+import { lotsAndDisposalsFromTrade } from '../generic/trade';
+
+export interface RedeemOptions {
+  txId: string;
+  pricesMap: ImmutableMap<{ string: List<Price> }>;
+  transactionsMap: ImmutableMap<{ string: 
