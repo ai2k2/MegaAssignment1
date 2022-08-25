565f889fcf08a816157f55c07df39d321cb951f2
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-08-25 08:15:21 +0000

    Add coverage.

--- src/accounting/compound_v2/repayBorrow.ts
+++ src/accounting/compound_v2/repayBorrow.ts
@@ -1,2 +1,47 @@
 
 import { Map as IMap, List } from 'immutable';
+
+import { Price, LocalCurrency, ImmutableMap, ITransaction, PriceMethod } from '../../types';
+import { lotsAndDisposalsFromWithdrawal } from '../generic/withdrawal';
+
+export interface RepayBorrowOptions {
+  txId: string;
+  pricesMap: ImmutableMap<{ string: List<Price> }>;
+  transactionsMap: ImmutableMap<{ string: List<ITransaction> }>;
+  priceMethod: PriceMethod;
+  localCurrency: LocalCurrency;
+}
+
+/*
+ * COMPOUND_REPAYBORROW
+ *
+ * Creates Lots and Disposals associated with a compound COMPOUND_REPAYBORROW.
+ */
+export const lotsAndDisposalsFromCompoundRepay = ({
+  txId,
+  transactionsMap,
+  pricesMap,
+  priceMethod,
+  localCurrency
+}: RepayBorrowOptions) => {
+  const repayment = transactionsMap.get(txId);
+  const updatedTransactionsMap = transactionsMap.set(
+    txId,
+    IMap({
+      tx_id: repayment.get('tx_id'),
+      tx_type: 'WITHDRAWAL',
+      timestamp: repayment.get('timestamp'),
+      withdrawal_code: repayment.get('repay_code'),
+      withdrawal_amount: repayment.get('repay_amount'),
+      fee_tx_ids: repayment.get('fee_tx_ids', List())
+    })
+  );
+  return lotsAndDisposalsFromWithdrawal({
+    txId,
+    transactionsMap: updatedTransactionsMap,
+    pricesMap,
+    priceMethod,
+    localCurrency,
+    isBorrowRepay: true
+  });
+};
---
+++ src/accounting/generic/deposit.ts
@@ -0,0 +1,76 @@
+
+import { BigNumber } from 'bignumber.js';
+import { Map as IMap, List } from 'immutable';
+
+import TaxLot from '../../taxLot';
+import { getFeeAmount } from '../fee';
+import { transactionUnixNumber, getPriceBigNumber } from '../helpers';
+import { Price, LocalCurrency, ImmutableMap, ITransaction, PriceMethod } from '../../types';
+
+export interface DepositOptions {
+  txId: string;
+  pricesMap: ImmutableMap<{ string: List<Price> }>;
+  transactionsMap: ImmutableMap<{ string: List<ITransaction> }>;
+  localCurrency: LocalCurrency;
+  priceMethod: PriceMethod;
+}
+
+/*
+ * DEPOSIT
+ * Creates a single TaxLot. Does not add income to the report,
+ * and does not create any taxable events (due to lack of Disposal).
+ * Creates a Disposal if there is a fee.
+ *
+ * USE CASES
+ * Fiat: add fiat to your portfolio balances. OR, deposit fiat at
+ * crypto exchange, and exchange charges you crypto fee (e.g. BNB).
+ * Capital asset: add crypto to portfolio balance. Basis is determined
+ * by the price on the date of deposit.
+ *
+ * TODO: allow for user to set their own basis. This would support
+ * the use case that a user remembers the basis but not when or
+ * where they acquired the captial asset.
+ *
+ */
+export const lotsAndDisposalsFromDeposit = ({
+  txId,
+  pricesMap,
+  transactionsMap,
+  priceMethod,
+  localCurrency
+}: DepositOptions) => {
+  const transactionPrices = pricesMap.get(txId);
+  const transaction = transactionsMap.get(txId);
+  const unixNumber = transactionUnixNumber(transaction);
+
+  // Get the basis amount to setup initial value for tax lot.
+  const lotCode = transaction.get('deposit_code').toUpperCase();
+  let lotAmount = new BigNumber(transaction.get('deposit_amount'));
+  const depositPrice = getPriceBigNumber(transactionPrices, lotCode, localCurrency);
+  let basisAmount = lotAmount.times(depositPrice);
+
+  // Adjust basis with applicable fees.
+  const taxableFeeAmount = getFeeAmount({
+    transaction,
+    pricesMap,
+    transactionsMap,
+    priceMethod,
+    localCurrency
+  });
+  basisAmount = BigNumber.sum(basisAmount, taxableFeeAmount);
+
+  return IMap({
+    taxLots: List([
+      new TaxLot({
+        unix: unixNumber,
+        assetCode: lotCode,
+        assetAmount: lotAmount,
+        basisCode: localCurrency,
+        basisAmount: basisAmount,
+        transactionId: txId,
+        isIncome: false
+      })
+    ]),
+    disposals: List()
+  });
+};
