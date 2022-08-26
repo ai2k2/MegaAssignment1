fbeefeefc537cd4a8d62a8a00eeed70404a4ac3d
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-08-26 22:41:09 +0000

    correct spelling mistake

--- src/accounting/generic/withdrawal.ts
+++ src/accounting/generic/withdrawal.ts
@@ -9,4 +9,9 @@ import { getFeeAmount } from '../fee';
 /*
  * WITHDRAWAL
  *
- * Creates a single Disposal (unl
+ * Creates a single Disposal (unless there are fees of other asset types).
+ * Creates a taxable event (if it is a captial asset).
+ *
+ * USE CASES:
+ * Fiat: withdraw cash cash from portfolio.
+ * Captial asset: spe
