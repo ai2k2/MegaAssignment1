eb9ac92f117194a05929377bf5b75f4422d425cc
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-08-26 16:22:40 +0000

    Update services

--- src/accounting/generic/withdrawal.ts
+++ src/accounting/generic/withdrawal.ts
@@ -1,4 +1,12 @@
 import { Map as IMap, List } from 'immutable';
 import { BigNumber } from 'bignumber.js';
 
-import { Price, LocalCurrency, ImmutableMap, ITransaction, PriceMethod } from '../
+import { Price, LocalCurrency, ImmutableMap, ITransaction, PriceMethod } from '../../types';
+import Disposal from '../../disposal';
+import { transactionUnixNumber, getPriceBigNumber } from '../helpers';
+import { getFeeAmount } from '../fee';
+
+/*
+ * WITHDRAWAL
+ *
+ * Creates a single Disposal (unl
