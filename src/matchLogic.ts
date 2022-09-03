582c15e7e70e4c183b50937bc973762d2f832292
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-09-03 09:00:04 +0000

    Oops, typo.

--- src/matchLogic.ts
+++ src/matchLogic.ts
@@ -1,3 +1,9 @@
 import moment from 'moment';
 import { Map as IMap, List } from 'immutable';
-import { BigNumber }
+import { BigNumber } from 'bignumber.js';
+
+import { HackedStack } from './types';
+import TaxLot from './taxLot';
+import Disposal from './disposal';
+
+export const lotSort = (lots: List<TaxLot>, costMethod: string): 
