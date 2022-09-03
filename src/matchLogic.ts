4e4fc510d0e4e0c0110d89ef6b752bc93f8d40f1
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-09-03 14:20:15 +0000

    Create search.html

--- src/matchLogic.ts
+++ src/matchLogic.ts
@@ -9,4 +9,8 @@ import Disposal from './disposal';
 export const lotSort = (lots: List<TaxLot>, costMethod: string): List<TaxLot> => {
   const fifoComparator = (a: TaxLot, b: TaxLot): number => a.unix - b.unix;
   const hifoComparator = (a: TaxLot, b: TaxLot): number =>
-    b.pricePerUnit.minus(a.pricePerUnit).toNumber(
+    b.pricePerUnit.minus(a.pricePerUnit).toNumber();
+  const lifoComparator = (a: TaxLot, b: TaxLot): number => b.unix - a.unix;
+  if (costMethod === 'FIFO') {
+    return lots.sort(fifoComparator);
+  } else if (costMethod
