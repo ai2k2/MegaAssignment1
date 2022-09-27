8171a658eefe4fcee25276edfb9090addf51e1d9
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-09-27 13:15:53 +0000

    Version bump to 0.3.7

--- src/report.ts
+++ src/report.ts
@@ -22,3 +22,175 @@ import {
   updateReportHoldingsFromLots,
   buildYearList,
   sortAccountingRecords,
+  applyUnmatchedSales,
+  setupAssetProperties,
+  updateReportIncomeFromLots,
+  sortDisposals,
+  groupBy
+} from './reportHelpers';
+import { bigNumberToString } from './utils/convert';
+import { makeLotsAndDisposals } from './accounting';
+import { validateOptions } from './validations';
+
+export const createReport = (options: TaxReportOptions): TaxReportOutput => {
+  validateOptions(options);
+  const {
+    prices,
+    transactions,
+    config: {
+      local_currency: localCurrency = 'USD',
+      price_method: priceMethod = 'BASE',
+      cost_basis_method: costBasisMethod = 'FIFO',
+      decimal_places: decimalPlaces = 2,
+      allow_lot_overlap: allowLotOverlap = true
+    } = {}
+  } = options;
+
+  const lotsAndDisposals = makeLotsAndDisposals({
+    prices: fromJS(prices),
+    transactions: fromJS(transactions),
+    priceMethod,
+    localCurrency
+  });
+  const disposalList: List<Disposal> = lotsAndDisposals.get('disposalList');
+  const taxLotList: List<TaxLot> = lotsAndDisposals.get('taxLotList');
+  // If there are no Disposals, then build the list of
+  // years from TaxLots instead.
+
+  const sortedDisposals = sortAccountingRecords({ records: disposalList });
+  const listToBuildYears =
+    sortedDisposals.size > 0 ? sortedDisposals : sortAccountingRecords({ records: taxLotList });
+  const yearList = buildYearList(listToBuildYears);
+  // Report iterable is a List of IMaps containing the year
+  // string and disposals filtered by year.
+  const reportIterable = buildReportIterable(sortedDisposals, yearList);
+  const taxLotsByAsset = groupBy(taxLotList, (lot) => lot.assetCode);
+  const taxReportOutput = buildReport({
+    reportIterable,
+    taxLotsByAsset,
+    costBasisMethod,
+    localCurrency,
+    allowLotOverlap
+  });
+  const reportWithNegHoldings = applyUnmatchedSales(taxReportOutput);
+  const plainJsReport = reportWithNegHoldings.toJS();
+  const plainNumberReport = bigNumberToString(plainJsReport, 10, decimalPlaces);
+
+  return {
+    report: plainNumberReport,
+    config: {
+      price_method: priceMethod,
+      cost_basis_method: costBasisMethod,
+      decimal_places: decimalPlaces,
+      local_currency: localCurrency,
+      allow_lot_overlap: allowLotOverlap
+    }
+  };
+};
+
+const buildReport = (options: {
+  reportIterable: List<any>;
+  taxLotsByAsset: IMap<string, List<TaxLot>>;
+  costBasisMethod: CostBasisMethod;
+  localCurrency: LocalCurrency;
+  allowLotOverlap: boolean;
+}): TaxReport => {
+  const {
+    reportIterable,
+    taxLotsByAsset,
+    costBasisMethod,
+    localCurrency,
+    allowLotOverlap
+  } = options;
+
+  const output = reportIterable.reduce((acc: IMap<any, any>, iterableYear: IMap<any, any>) => {
+    // for each year
+    const reportYearSetup = acc.get('report').set(
+      iterableYear.get('year'),
+      fromJS({
+        income: [],
+        long: [],
+        short: [],
+        unmatched: [],
+        lost: [],
+        interest_income: [],
+        assets: {}
+      })
+    );
+
+    const reportIncomeSetup = updateReportIncomeFromLots(
+      reportYearSetup,
+      acc.get('lots'),
+      iterableYear.get('year')
+    );
+
+    const reportWithHoldingChanges = setupAssetProperties(
+      iterableYear.get('year'),
+      reportIncomeSetup,
+      iterableYear.get('disposals'),
+      acc.get('lots')
+    );
+
+    const resultingReportAndLots = buildReportYear({
+      disposals: iterableYear.get('disposals'),
+      lots: acc.get('lots'),
+      report: reportWithHoldingChanges,
+      costBasisMethod,
+      localCurrency,
+      allowLotOverlap
+    });
+
+    const report = updateReportHoldingsFromLots(resultingReportAndLots, iterableYear.get('year'));
+
+    return acc.set('report', report).set('lots', resultingReportAndLots.get('lots'));
+  }, IMap({ lots: taxLotsByAsset, report: IMap() }));
+
+  return output.get('report');
+};
+
+const buildReportYear = (
+  options: BuildReportYearOptions
+): ImmutableMap<{ report: TaxReportYear; lots: List<TaxLot> }> => {
+  const { disposals, lots, report, costBasisMethod, localCurrency, allowLotOverlap } = options;
+  const sortedDisposals = sortDisposals({ records: disposals });
+  // for each Disposal in year...
+  return sortedDisposals.reduce(
+    (acc: ImmutableMap<{ report: TaxReportYear; lots: List<TaxLot> }>, disposal: Disposal) => {
+      const disposalAsset = disposal.assetCode;
+      const disposalLots = acc.get('lots').get(disposalAsset, List());
+      const beforeDisposal = (lot: TaxLot) => {
+        if (allowLotOverlap) {
+          return lot.unix <= disposal.unix;
+        } else {
+          return lot.unix < disposal.unix;
+        }
+      };
+      const lotsForStack = disposalLots.filter(beforeDisposal);
+      const sortedLots = lotSort(lotsForStack, costBasisMethod);
+      const remainingLots = disposalLots.filterNot(beforeDisposal);
+      const res = matchLots({
+        disposal,
+        lotsForStack: sortedLots,
+        report: acc.get('report'),
+        localCurrency
+      });
+      const lotsDepleted = res.get('lotsDepleted');
+      const currentReport = res.get('currentReport');
+
+      const assetLots = lotsDepleted.concat(remainingLots);
+      const allLots = acc.get('lots').set(disposalAsset, assetLots);
+
+      return acc.set('report', currentReport).set('lots', allLots);
+    },
+    IMap({
+      report: report,
+      lots: lots
+    })
+  );
+};
+
+// This is the core logic of the tax model.
+// For each Disposal, match it against a TaxLot until the Disposal is reduced to a zero value.
+const matchLots = (options: {
+  disposal: Disposal;
+  lotsForStack: List<TaxLot>;
