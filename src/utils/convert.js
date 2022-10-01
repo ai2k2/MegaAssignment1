1b035db465348014fca4ca2a650a469fd94c43c8
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-10-01 10:45:16 +0000

    Add Indonesian translation

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -1,1 +1,8 @@
-import { BigNumber } fro
+import { BigNumber } from 'bignumber.js';
+
+// setup main method
+export function bigNumberToString(obj, base, places = 2) {
+  // setup base
+  base = base || 10;
+
+  // check if obj is type object, not an array and does not have BN 
