7f68ccebb494ce101b89618c0e676e1dbda07f37
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-10-01 15:01:41 +0000

    New README

--- src/utils/convert.js
+++ src/utils/convert.js
@@ -5,4 +5,7 @@ export function bigNumberToString(obj, base, places = 2) {
   // setup base
   base = base || 10;
 
-  // check if obj is type object, not an array and does not have BN 
+  // check if obj is type object, not an array and does not have BN properties
+  if (typeof obj === 'object' && obj !== null && !Array.isArray(obj) && !('lessThan' in obj)) {
+    // move through plain object
+    Object.keys(obj).forEach(function(key) {
