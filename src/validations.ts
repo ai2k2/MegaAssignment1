16594cb6d6eee7c100d36a2081332d32a34eb7ee
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-10-02 21:11:45 +0000

    Update version to 0.1.2

--- src/validations.ts
+++ src/validations.ts
@@ -5,4 +5,12 @@ export const validateOptions = (options: TaxReportOptions): void => {
   const {
     prices,
     transactions,
-    confi
+    config: {
+      local_currency = 'USD',
+      price_method = 'BASE',
+      cost_basis_method = 'FIFO',
+      decimal_places = 2
+    } = {}
+  } = options;
+  // Assert params are not null or undefined.
+  c
