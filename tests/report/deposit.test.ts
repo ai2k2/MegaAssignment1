60169e0caf2c553dac26827c64f41f80573b4332
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-12-14 15:40:30 +0000

    Fix build path

--- tests/report/deposit.test.ts
+++ tests/report/deposit.test.ts
@@ -1,2 +1,11 @@
 import createReport from '../../src/index';
-import { PriceMethod, CostBasisMethod, Price 
+import { PriceMethod, CostBasisMethod, Price } from '../../src/types';
+import {
+  taxReportFactory,
+  depositFactory,
+  tradeFactory,
+  withdrawalFactory
+} from '../utils/factories';
+
+describe('deposit assets', () => {
+  describe(
