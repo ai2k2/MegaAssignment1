a324e5ec2b417fb93555930067cab5ec82fc31ca
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2022-12-14 23:09:33 +0000

    apache license

--- tests/report/deposit.test.ts
+++ tests/report/deposit.test.ts
@@ -12,4 +12,11 @@ describe('deposit assets', () => {
     const deposit_1 = depositFactory({
       timestamp: '2018-01-01T01:00:00Z',
       deposit_amount: '1',
-      deposit_code: 'BTC
+      deposit_code: 'BTC'
+    });
+    const deposit_2 = depositFactory({
+      timestamp: '2018-01-02T01:00:00Z',
+      deposit_amount: '1',
+      deposit_code: 'BTC'
+    });
+    const deposit_3 = depositFactor
