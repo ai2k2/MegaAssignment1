c2cf775d67293850e9cc441c4396164c3202557a
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2023-03-19 21:04:56 +0000

    Remove some unused variables.

--- tests/report/lost.test.ts
+++ tests/report/lost.test.ts
@@ -311,3 +311,33 @@ describe('lost crypto or fiat', () => {
               date_sold: '2018-01-01T01:00:00Z',
               proceeds: '1',
               tx_id_sale: lost.tx_id
+            }
+          ],
+          long: [],
+          income: [],
+          short: [],
+          lost: [
+            {
+              asset: 'USD',
+              asset_amount: '1',
+              cost_basis: '0',
+              date_acquired: '2018-01-01T01:00:00Z',
+              date_sold: '2018-01-01T01:00:00Z',
+              proceeds: '1',
+              tx_id_sale: lost.tx_id
+            }
+          ],
+          interest_income: []
+        }
+      },
+      config: {
+        local_currency: 'USD',
+        price_method: 'BASE',
+        cost_basis_method: 'FIFO',
+        decimal_places: 2,
+        allow_lot_overlap: true
+      }
+    });
+    expect(received).toEqual(expected);
+  });
+});
