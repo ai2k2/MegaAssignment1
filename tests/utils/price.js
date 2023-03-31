bbd390eda600dece99e3b8621ded7c61434f5553
ai2k2 <95246271+ai2k2@users.noreply.github.com>
2023-03-31 17:44:11 +0000

    USB: echi-dbgp: increase the controller wait time to come out of halt.

--- tests/utils/price.js
+++ tests/utils/price.js
@@ -1,1 +1,5 @@
-import { BigNumber } fr
+import { BigNumber } from 'bignumber.js';
+
+export const impliedPrice = ({ amountPriced, amountUnpriced, price }) => {
+  return new BigNumber(amountPriced)
+    .dividedBy(new BigNumber(amountUnpriced)
